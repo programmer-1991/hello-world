d905419e2508b28ee249ce0d77a99e1d0e9107b2
programmer-91 <aminrhimi.1991@gmail.com>
2024-02-16 22:16:13 +0000

    Add heroku hostname to allowed hosts

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!sprv$@q549hr9dh@wu(5&x)s_wi&_%5z3v^wjkwjoa+cyjfeo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-programmer19-helloworld-gjh9m69a9ou.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-programmer19-helloworld-gjh9m69a9ou.ws-eu108.gitpod.io', '.herokuapp.com']
 
 # Application definition
 
